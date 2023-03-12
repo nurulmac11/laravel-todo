0b40f6ca741e5560ccb73c66147eba1e9e2f9513
Nurullah Macun <nurullah201@gmail.com>
2023-03-12 15:17:31 +0300

    add bootstrap

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,9 +6,14 @@ import router from './router'
 
 import './assets/main.css'
 
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "bootstrap/dist/js/bootstrap.js"
