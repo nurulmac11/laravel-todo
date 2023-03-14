7293178e26326b1bba5557daa64c8a6f97a8fac8
Nurullah Macun <nurullah201@gmail.com>
2023-03-14 11:43:16 +0300

    send priority string

--- backend/app/Models/Todo.php
+++ backend/app/Models/Todo.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
@@ -34,13 +35,24 @@ class Todo extends Model
 
     public function getPriorityAttribute($value)
     {
-        return TODO::$priority_list[$value];
+        if (is_int($value)) {
+            return TODO::$priority_list[$value];
+        }
+        return $value;
     }
+
+    protected function priority(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $this->getPriorityAttribute($value),
+        );
+    }
+
 
     static public function getTodos(int $user_id)
     {
-        return DB::table('todos')
-            ->select(['todo_groups.name as group_name', 'todos.name as title',
+        return Todo::select(
+            ['todo_groups.name as group_name', 'todos.name as title',
                 "todos.id",
                 "todos.description",
                 "todos.user_id",
@@ -50,10 +62,8 @@ class Todo extends Model
                 "todos.due_date",
                 "todos.created_at",
                 "todos.updated_at",
-            ])
-            ->leftJoin('todo_groups', 'todo_groups.id', '=', 'todos.todo_group_id')->where('todos.user_id', '=', $user_id)
-            ->orderBy('todos.id', 'DESC')
-            ->get();
+            ]
+        )->join('todo_groups', 'todo_groups.id', '=', 'todos.todo_group_id')->orderBy('todos.id', 'DESC')->get();
     }
 
     static public function getPriorities() {
