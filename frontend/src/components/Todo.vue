2aaaf0ce0f5697f67c150ab68c2f16fc5336d6ea
Nurullah Macun <nurullah201@gmail.com>
2023-03-17 19:08:33 +0300

    edit all attrs

--- frontend/src/components/Todo.vue
+++ frontend/src/components/Todo.vue
@@ -2,6 +2,8 @@
 
 import {useTodoStore} from "@/stores/todo";
 import {toRefs, ref} from "vue";
+import TodoGroupSelector from "@/components/TodoGroupSelector.vue";
+import PrioritySelector from "@/components/PrioritySelector.vue";
 
 const props = defineProps(['todo'])
 
@@ -11,6 +13,9 @@ const todoStore = useTodoStore();
 
 const editFlag = ref(false);
 const editTodo = ref(todo?.value.title);
+const editGroup = ref(todo?.value.todo_group_id);
+const editPriority = ref(todo?.value.priority);
+const editDueDate = ref(todo?.value.due_date);
 const completedToggle = () => {
   if (todo?.value.completed == 0) {
     todoStore.completeTodo(todo.value.id);
@@ -27,7 +32,7 @@ const remove = () => {
 const edit = () => {
   // Update if we are saving
   if (editFlag.value) {
-    todoStore.updateTodo(todo?.value.id, editTodo.value, todo?.value.todo_group_id, todo?.value.priority, todo?.value.due_date);
+    todoStore.updateTodo(todo?.value.id, editTodo.value, editGroup.value, editPriority.value, editDueDate.value);
   }
 
   editFlag.value = !editFlag.value;
@@ -38,11 +43,24 @@ const edit = () => {
 <template>
   <li class="list-group-item d-flex justify-content-between align-items-start" :key="todo.id">
     <div class="ms-2 me-auto">
-      <div class="fw-bold" :class="{'todo-done': todo.completed}" v-if="!editFlag">{{ todo.title }}</div>
-      <input v-model="editTodo" type="text" class="form-control" v-else />
-      <p>Group: {{ todo.group_name }}</p>
-      <p>Priority: {{ todo.priority_string }}</p>
-      <p>Due date: {{ todo.due_date }}</p>
+      <div v-if="!editFlag">
+        <div class="fw-bold" :class="{'todo-done': todo.completed}">{{ todo.title }}</div>
+        <p>Group: {{ todo.group_name }}</p>
+        <p>Priority: {{ todo.priority_string }}</p>
+        <p>Due date: {{ todo.due_date }}</p>
+      </div>
+      <div v-else>
+        <input v-model="editTodo" type="text" class="form-control" />
+        <TodoGroupSelector
+            v-model="editGroup"
+        />
+        <PrioritySelector
+            v-model="editPriority"
+        />
+        <VueDatePicker v-model="editDueDate" placeholder="Select due date" :enable-time-picker="false"
+                       :min-date="new Date()" model-type="yyyy-MM-dd" auto-apply/>
+      </div>
+
     </div>
     <span class="badge bg-primary"><button
         class="btn"
